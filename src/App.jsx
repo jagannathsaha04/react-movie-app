015e9086a3b6ab6a2058fdb14f06cba91d215d88
Jagannath Saha <hello.jagannathsaha@gmail.com>
2025-02-24 10:48:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MovieCard from './components/MovieCard'
+import Home from './pages/Home';
+
+function App() {
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+  
+}
+
+export default App;
