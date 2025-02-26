a4dc7b1ab91ff6f14784c65f4d40d69b2995dbdf
Jagannath Saha <hello.jagannathsaha@gmail.com>
2025-02-26 23:10:09 +0530

    made the favourites page

---
+++ src/New folder/Favourites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favourites.css"
+
+function Favourites() {
+    return <div className="favourites-empty">
+        <h2>No Favourites Yet.</h2>
+        <p>Start adding movies to your favourites and they will appear here.</p>
+    </div>
+}
+
+export default Favourites
--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,10 +1,31 @@
-import "../css/Favourites.css"
+import "../css/Favourites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favourites() {
-    return <div className="favourites-empty">
-        <h2>No Favourites Yet.</h2>
-        <p>Start adding movies to your favourites and they will appear here.</p>
-    </div>
+    const { favourites } = useMovieContext();
+
+    // Show empty state when no favourites are available
+    if (favourites.length === 0) {
+        return (
+            <div className="favourites-empty">
+                <h2>No Favourites Yet.</h2>
+                <p>Start adding movies to your favourites and they will appear here.</p>
+            </div>
+        );
+    }
+
+    // Show favourites when available
+    return (
+        <div className="favourites-container">
+            <h2>Your Favourite Movies</h2>
+            <div className="movies-grid">
+                {favourites.map(movie => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default Favourites
+export default Favourites;
